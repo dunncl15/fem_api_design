1bf6129af7eeee5c39c7440b6471911ee52a4b81
Charles Dunn <dunnc1551@gmail.com>
2019-06-22 15:37:34 -0600

    Complete lesson 1.

--- src/server.js
+++ src/server.js
@@ -12,4 +12,15 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/api/data', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/api/data', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => console.log('server running on port 3000'))
+}
