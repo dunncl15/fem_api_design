9556718965ac437fd3963c90f923506ba929852d
Charles Dunn <dunnc1551@gmail.com>
2019-06-23 20:08:37 -0600

    Mount sub routes.

--- src/server.js
+++ src/server.js
@@ -1,25 +1,34 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
-import cors from 'cors'
-import { connect } from './utils/db'
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import { connect } from './utils/db';
+import config from './config';
 
-export const app = express()
+import itemRouter from './resources/item/item.router';
+import listRouter from './resources/list/list.router';
+import userRouter from './resources/user/user.router';
 
-app.disable('x-powered-by')
+export const app = express();
 
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.use('/api/item', itemRouter);
+app.use('/api/list', listRouter);
+app.use('/api/user', userRouter);
 
 export const start = async () => {
   try {
-    await connect()
+    await connect();
     app.listen(config.port, () => {
-      console.log(`REST API on http://localhost:${config.port}/api`)
-    })
+      console.log(`REST API on http://localhost:${config.port}/api`);
+    });
   } catch (e) {
-    console.error(e)
+    console.error('CONNECT ERROR: ', e);
   }
-}
+};
